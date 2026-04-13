75205618b5defdb32660e766acad9b3c36ed3610
Aditya Suryawanshi <adityassuryawanshi25@gmail.com>
2026-04-13 16:50:42 +0530

    Create main11.py

---
+++ main11.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+def home() :
+  return {"message" : "Hello.. , WELCOME "}
+
+@app.get("/about")
+def about():
+  return {"message" : "About Us.. "}
+
+@app.get("/contact")
+def contact():
+  return {"message" : "Contact us Here"}
+
+@app.get("/skills")
+def skills():
+  return {"message" : "Python"}
+
+@app.get("/projects")
+def projects():
+  return {"message" : "Nexovia AI , AI chatbots"}
