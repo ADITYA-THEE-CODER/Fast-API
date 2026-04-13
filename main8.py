5b33db2ec7d9c0fa2f8183449b4f7c0d96622208
Aditya Suryawanshi <adityassuryawanshi25@gmail.com>
2026-04-13 16:46:11 +0530

    Create main8.py

---
+++ main8.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+def home :
+  return {"message" : "Hello.. , WELCOME "}
+
+
+@app.get("/about")
+def about:
+  return {"message" : "About Us.. "}
+
+
+@app.get("/contact")
+def contact:
+  return {"message" : "Contact us Here"}
+
+@app.get("/skills")
+def skills:
+  return {"message" : "Python"}
